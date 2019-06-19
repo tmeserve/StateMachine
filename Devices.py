dcb69097db2cc4766652ba2215b6dcadf01f79c0
Tyler <tyler.m.meserve@gmail.com>
2019-06-19 10:37:28 -0500

    Finished the task

--- Devices.py
+++ Devices.py
@@ -8,11 +8,21 @@ class SimpleDevice(object):
         self.start_time = datetime.datetime.now()
         self.datetime_format = "%Y-%m-%d %H:%M%S.%f"
         self.start_time = self.start_time.replace(microsecond=0)
+        self.printed = False
+        self.start = True
         # self.start_time = self.start_time.replace(second=0)
 
     def run(self):
         current_time = datetime.datetime.now()
         diff = current_time - self.start_time
-        if diff.seconds % self.delay == 0:
+
+        if current_time.second == self.start_time.second:
+            self.start = False
+            return
+
+        if diff.seconds % self.delay == 0 and not self.printed and not self.start:
             print(self.string)
-        
+            print("It's been {0} seconds since the last print. The elapsed time is {1} second(s).".format(self.delay, diff.seconds))
+            self.printed = True
+        elif diff.seconds % self.delay != 0 and self.printed:
+            self.printed = False
